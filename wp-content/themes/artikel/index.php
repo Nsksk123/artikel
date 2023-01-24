695a5bb826650462177bb98f506952ab5807e00e
Nsksh <nathanaelsltg123@gmail.com>
2023-01-24 15:33:56 +0700

    membuat artikel

--- wp-content/themes/artikel/index.php
+++ wp-content/themes/artikel/index.php
@@ -9,9 +9,9 @@
                 <h4>Pilih Kategori</h4>
                 <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
                     <option selected>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    <option value="1">News Updates</option>
+                    <option value="2">Covid-19 Evens</option>
+                    <option value="3">Each selected editor</option>
                 </select>
             </div>
         </div>
@@ -48,9 +48,18 @@
         wp_reset_postdata();
 
     ?>
-    
+
 </div>
         </div>
     </section>
+
+    <?php
+    $db = mysqli_connect("127.0.0.1", "admin1", "1admin", "artikel");
+
+
+    $id = mysqli_query($db, 'SELECT * FROM wp_posts');
+    $id_1 = mysqli_fetch_assoc($id);
+    echo $id_1['id']
+    ?>
 
 <?php get_footer();?>
